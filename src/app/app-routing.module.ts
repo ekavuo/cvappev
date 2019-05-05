f423a88845f2c3d0b6ed586326aeb485bb6d943f
ekavuo <47452245+ekavuo@users.noreply.github.com>
2019-05-05 22:39:48 +0300

    CVappEV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'project', loadChildren: './project/project.module#ProjectPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'extra', loadChildren: './extra/extra.module#ExtraPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+ selector: 'app-root',
+ templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+ public appPage = [
+ {
+ title: 'Project',
+ url: '/project',
+ icon: 'briefcase'
+ },
+ {
+ title: 'Extra Curriculum	activities',
+ url: '/extra',
+ icon: 'star'
+ }
+ ];
+ constructor(
+ private platform: Platform,
+ private splashScreen: SplashScreen,
+ private statusBar: StatusBar
+ ) {
+ this.initializeApp();
+ }
+ initializeApp() {
+ this.platform.ready().then(() => {
+ this.statusBar.styleDefault();
+ this.splashScreen.hide();
+ });
+ }
 }
